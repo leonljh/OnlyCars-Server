138639a7a92978a258fbd30f26792c2bf371469f
leonljh <leonlowjunhao@hotmail.com>
2021-03-15 21:41:09 +0800

    wrapped all api requests in a single method

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
+var dataset1 = "";
 
+
+function init(){
 //this is to get carpark details (carparkID, longitude, latitude)
-// axios.get('https://data.gov.sg/api/action/datastore_search?resource_id=139a3035-e624-4f56-b63f-89ae28d4ae4c')
-//     .then(response => {
-//         console.log(response.data.result.records);
-//     })
-//     .catch(error => {
-//         console.log(error);
-//     })
-
+axios.get('https://data.gov.sg/api/action/datastore_search?resource_id=139a3035-e624-4f56-b63f-89ae28d4ae4c')
+    .then(response => {
+        console.log(response.data.result.records);
+    })
+    .catch(error => {
+        console.log(error);
+    })
 
 //this is to get carpark number of lots available. with respect to the carparkID
-// axios.get('https://api.data.gov.sg/v1/transport/carpark-availability')
-//     .then(response => {
-//         console.log(response.data.items[0].carpark_data[0].carpark_info);
-//     })
-//     .catch(error => {
-//         console.log(error);
-//     })
+axios.get('https://api.data.gov.sg/v1/transport/carpark-availability')
+    .then(response => {
+        console.log(response.data.items[0].carpark_data[0].carpark_info);
+    })
+    .catch(error => {
+        console.log(error);
+    })
 
 //URA carpark API call for all carparks
 axios.get('https://www.ura.gov.sg/uraDataService/invokeUraDS?service=Car_Park_Details', {
@@ -34,6 +36,9 @@ axios.get('https://www.ura.gov.sg/uraDataService/invokeUraDS?service=Car_Park_De
     .catch(error => {
         console.log(error);
     })
+
+}
+
 
 /* 
 now to setup the database using carparkId as the key, and place them as:
